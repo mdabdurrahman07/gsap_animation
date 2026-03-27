820ca5f40e72350777c5e8003583030be90a614f
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-03-27 21:18:27 +0600

    This challenge is about small card film with from and to logic

---
+++ pages/11b_challenge-from/script.js
@@ -0,0 +1,21 @@
+import gsap from "gsap";
+
+const card = document.querySelector("#card");
+
+card.addEventListener("mouseenter", () => {
+  gsap.to(card, {
+    rotateY: 180,
+    duration: 0.6,
+    ease: "power2.inOut"
+  });
+});
+
+
+card.addEventListener("mouseleave", () => {
+  gsap.to(card, {
+    rotateY: 0,
+    duration: 0.6,
+    ease: "power2.inOut"
+  });
+});
+
